8b97f642ac36356dee8532409f6ee860c2786130
Arthur Puyou <arthur@puyou.fr>
2019-08-06 18:06:48 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,221 @@
+const gphoto2 = require('@nouvellecuisine/gphoto2');
+
+const Toolbar = require('./toolbar');
+
+// Target video speed (TARGET_FRAME_TIME=1000/FPS, set 0 for maximum speed)
+const TARGET_FRAME_TIME = Math.round(1000/25);
+
+class NCCamera {
+  GPhoto = null;
+  camera = null;
+  running = false;
+  frameStart = 0;
+  frameEnd = 0;
+  autostart = true;
+  callback = null;
+  toolbar = null;
+
+  constructor({ autostart = true, callback }) {
+    this.autostart = autostart;
+    this.callback = callback;
+
+    this.toolbar = new Toolbar(this);
+
+    this.hotplug();
+
+    window.onbeforeunload = this.stopLive;
+  }
+
+  hotplug = () => {
+    this.setup()
+    .then(() => {
+      return this.getSettings()
+      .then((settings) => {
+        console.log('Got settings', settings);
+        if (settings) {
+          if (this.autostart) {
+            return this.startLive();
+          }
+        }
+        else {
+          setTimeout(this.hotplug, 1000);
+        }
+      }, (error) => {
+        console.warn('Could not talk to camera', error);
+        setTimeout(this.hotplug, 1000);
+      });
+    }, (error) => {
+      console.error('NCCameraClient', 'Failed opening camera:', error);
+      setTimeout(this.hotplug, 1000);
+    });
+  }
+
+  setup = () => {
+    console.log('NCCamera', 'Doing setup', this.GPhoto);
+
+    if (this.camera !== null) {
+      console.log('NCCamera', 'Existing camera object discarded');
+      delete this.camera;
+      this.camera = null;
+    }
+
+    if (this.GPhoto !== null) {
+      console.log('NCCamera', 'Existing GPhoto instance discarded');
+      delete this.GPhoto;
+      this.GPhoto = null;
+    }
+
+    console.log('NCCamera', 'Initialize GPhoto');
+    this.GPhoto = new gphoto2.GPhoto2();
+    this.GPhoto.setLogLevel(1);
+    this.GPhoto.on('log', function (level, domain, message) {
+      console.log('NCCamera', domain, message);
+    });
+
+    return new Promise((resolve, reject) => {
+      this.GPhoto.list((list) => {
+        if (list.length === 0) {
+          console.warn('NCCamera', 'No camera found');
+          reject('No camera found');
+          return;
+        }
+
+        this.camera = list[0];
+        console.log('NCCamera', 'Found', this.camera.model);
+
+        resolve(this.camera);
+      });
+    });
+  }
+
+  startLive = (event) => {
+    if (this.running) {
+      console.warn('NCCamera', 'Live is already running');
+      return;
+    }
+
+    if (this.camera === null) {
+      console.warn('NCCamera', 'No camera found, please check camera and call setup');
+      return;
+    }
+
+    console.log('NCCamera', 'Starting live');
+    this.running = true;
+    this.runLive();
+  }
+
+  stopLive = () => {
+    if (!this.running) {
+      console.warn('NCCamera', 'Live is not running');
+      return;
+    }
+
+    console.log('NCCamera', 'Stopping live');
+    this.running = false;
+  }
+
+  runLive = () => {
+    if (this.camera === null) {
+      console.warn('NCCamera', 'No camera detected');
+      return;
+    }
+
+    this.frameStart = Date.now();
+
+    this.camera.takePicture({
+      preview: true,
+      download: true,
+    }, (er, data) => {
+      if (er < 0) {
+        console.warn('NCCamera', 'Error received from camera, stopping live', er);
+        this.stopLive();
+        this.hotplug();
+      }
+      else {
+        if (data) {
+          if (this.callback) {
+            this.callback(data);
+          }
+        }
+        else {
+          console.warn('NCCamera', 'Empty data received from camera', er);
+        }
+
+        if (this.running) {
+          this.frameEnd = Date.now();
+          const work = this.frameEnd - this.frameStart;
+          const sleep = Math.max(TARGET_FRAME_TIME - work, 0);
+          setTimeout(this.runLive, sleep);
+        }
+      }
+    });
+  }
+
+  capture = () => {
+    if (this.camera === null) {
+      console.warn('NCCamera', 'No camera detected');
+      return;
+    }
+
+    return new Promise((resolve, reject) => {
+      this.camera.takePicture({
+        preview: false,
+        download: true,
+      }, (er, data) => {
+        if (er < 0) {
+          console.warn('NCCamera', 'Error received from camera, stopping live', er);
+          reject(er);
+        }
+        else {
+          if (data) {
+            resolve(data);
+            return;
+          }
+          else {
+            console.warn('NCCamera', 'Empty data received from camera', er);
+          }
+        }
+      });
+    });
+  }
+
+  autofocus = () => {
+    return this.setSetting('cancelautofocus', 1)
+      .then(this.setSetting('autofocusdrive', 1));
+  }
+
+  getSettings = () => {
+    if (this.camera === null) {
+      console.warn('NCCamera', 'No camera detected');
+      return Promise.reject('No camera detected');
+    }
+
+    console.log('NCCamera', 'Get settings');
+    return new Promise((resolve) => {
+      this.camera.getConfig((er, settings) => {
+        console.log('NCCamera', 'Settings retrieved');
+        resolve(settings);
+      });
+    });
+  }
+
+  setSetting = (key, value) => {
+    if (this.camera === null) {
+      console.warn('NCCamera', 'No camera detected');
+      return Promise.reject('No camera detected');
+    }
+
+    return new Promise((resolve, reject) => {
+      this.camera.setConfigValue(key, value, (er) => {
+        if (er < 0) {
+          reject('Cannot set config', er);
+        }
+        else {
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+module.exports = NCCamera;
---
+++ toolbar.js
@@ -0,0 +1,80 @@
+const debugToolbarPrefix = 'nccamera_debugtoolbar';
+const debugToolbar = `
+  <style type="text/css">
+    #${debugToolbarPrefix} {
+      position:absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      background: #000;
+      color: #fff;
+      font-family: sans-serif;
+    }
+    #${debugToolbarPrefix} button {
+      font-size: 1.5em;
+      color: #FFF;
+      background: #0CADA7;
+      padding: 10px 30px;
+      border-radius: 5px;
+      margin: 10px 0;
+      transition: all 0.2s ease-in-out;
+    }
+    #${debugToolbarPrefix} button:hover {
+      background: #0A948F;
+    }
+  </style>
+  <div id="${debugToolbarPrefix}">
+    <div id="${debugToolbarPrefix}_status">Not connected</div>
+    <div>
+      White Balance: <select name=""><option>A</option></select> |
+      Speed: <select name=""><option>A</option></select> |
+      ISO: <select name=""><option>A</option></select> |
+      Aperture: <select name=""><option>A</option></select> |
+      Image Size: <select name=""><option>A</option></select>
+    </div>
+    <div>
+      <button id="${debugToolbarPrefix}_start">Start live</button>
+      <button id="${debugToolbarPrefix}_stop">Stop live</button>
+      <button id="${debugToolbarPrefix}_focus">Focus</button>
+    </div>
+  </div>
+`;
+
+class Toolbar {
+  container = null;
+  nccamera = null;
+
+  constructor(nccamera) {
+    this.nccamera = nccamera;
+
+    document.addEventListener('keyup', (event) => {
+      if (event.keyCode === 67) {
+        if (this.container) {
+          this.hide();
+        } else {
+          this.show();
+        }
+      }
+    });
+  }
+
+  show = () => {
+    this.container = document.createElement('div');
+    this.container.innerHTML = debugToolbar;
+    document.body.appendChild(this.container);
+
+    document.getElementById(`${debugToolbarPrefix}_start`)
+      .addEventListener('click', this.nccamera.startLive);
+    document.getElementById(`${debugToolbarPrefix}_stop`)
+      .addEventListener('click', this.nccamera.stopLive);
+    document.getElementById(`${debugToolbarPrefix}_focus`)
+      .addEventListener('click', this.nccamera.autofocus);
+  }
+
+  hide = () => {
+    document.body.removeChild(this.container);
+    this.container = null;
+  }
+}
+
+module.exports = Toolbar;
